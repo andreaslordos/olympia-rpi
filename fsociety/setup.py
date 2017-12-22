3e7160a6194289b463021a0c87a179ca0d983b84
Andreas Lordos <andreasglordos@gmail.com>
2017-12-22 22:30:11 +0200

    Update setup.py

--- fsociety/setup.py
+++ fsociety/setup.py
@@ -1,22 +1,35 @@
 from os import getcwd
 from sys import platform
 from os import system
+import ipgetter
+import requests
 
 operating_system=platform
 dir=getcwd()
+IP=ipgetter.myip()
+url='http://freegeoip.net/json/'+IP
+r=requests.get(url)
+js=r.json()
+city=js['city']
+country=js['country_name']
+fullStr=city+" "+country
 
 if "linux" in operating_system or operating_system=="Darwin":
     f1=open("code/dir.txt","w")
     f2=open("resources/dir.txt", "w")
     f3=open("hotwords/dir.txt","w")
+    f4=open("resources/location.txt","w")
 elif operating_system[0:3]=="win":
     f1=open("code\\dir.txt","w")
     f2=open("resources\\dir.txt", "w")
     f3=open("hotwords\\dir.txt","w")
-    
+    f4=open("resources\\location.txt","w")
+
 f1.write(dir)
 f2.write(dir)
 f3.write(dir)
+f4.write(fullStr)
+f4.close()
 f3.close()
 f2.close()
 f1.close()
