f3acef165b2fea02dcd0f7690ec30a22d8472cbf
Andreas Lordos <andreasglordos@gmail.com>
2018-05-07 11:43:18 +0300

    Made API key optional
    Apparently pafy provides a default API key. The more you know.

--- code/play_music.py
+++ code/play_music.py
@@ -44,14 +44,14 @@ def playMusic(userinput, choiceIsPure):
         return p
 
     def getCorrectLink(urls):
-        pafy.set_api_key(INSERT KEY HERE)
+        #pafy.set_api_key(INSERT KEY HERE)
         for url in urls:
             if len(url)==43 and url[:32]=='https://www.youtube.com/watch?v=':
                 song=pafy.new(url[32:])
                 return url
 
     #Main Program
-    pafy.set_api_key(INSERT KEY HERE)
+    #pafy.set_api_key(INSERT KEY HERE)
     if choiceIsPure==False:
         triggerword=["play","on"]
         if "that goes like" in userinput.lower() or "that goes" in userinput.lower() or "lyrics" in userinput.lower():
